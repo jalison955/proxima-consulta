aa9a7eed3f88f46ef292b290ed783d7a10cef011
Alison Pinheiro <j.alison955@gmail.com>
2025-05-27 16:40:29 -0400

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center bg-zinc-800 text-5xl text-white">
+    <div className="flex h-screen flex-col items-center justify-center bg-zinc-800 text-5xl text-white">
       <h1>SaaS Próxima Consulta</h1>
+      <Button>Entrar</Button>
     </div>
   );
 }
