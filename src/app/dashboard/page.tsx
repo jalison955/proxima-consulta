ae5703b09f36fabfc5b40de8bde0c35621d9d488
Alison Pinheiro <j.alison955@gmail.com>
2025-05-28 19:13:11 -0400

    feat: add funcionality to create clinics

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,9 @@
-//import { eq } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-//import { db } from "@/db";
-//import { usersToClinicsTable } from "@/db/schema";
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import SignOutButton from "./components/sign-out-button";
@@ -16,12 +16,14 @@ const DashboardPage = async () => {
   if (!session?.user) {
     redirect("/authentication");
   }
-  /* const clinics = await db.query.usersToClinicsTable.findMany({
+
+  const clinics = await db.query.usersToClinicsTable.findMany({
     where: eq(usersToClinicsTable.userId, session.user.id),
   });
   if (clinics.length === 0) {
     redirect("/clinic-form");
-  }*/
+  }
+
   return (
     <div>
       <h1>Dashboard</h1>
--- src/db/index.ts
+++ src/db/index.ts
@@ -2,4 +2,6 @@ import "dotenv/config";
 
 import { drizzle } from "drizzle-orm/node-postgres";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+import * as schema from "./schema"; //importa o schema para tornar o db visivel para quem chama o db
+
+export const db = drizzle(process.env.DATABASE_URL!, { schema });
