2827390637b88cbe76f5d2399df638de7b40fef7
Alison Pinheiro <j.alison955@gmail.com>
2025-05-28 11:55:50 -0400

    feat: : add login and signup  form

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,25 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import LoginForm from "./components/login-form";
+import SignupForm from "./components/signup-form";
+
+const Login = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="register">Criar conta</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <LoginForm />
+        </TabsContent>
+        <TabsContent value="register">
+          <SignupForm />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default Login;
